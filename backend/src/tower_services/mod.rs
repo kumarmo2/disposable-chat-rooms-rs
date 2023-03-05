b4fd6a7ed5c2f5f76a9f7fa1fca3d0706a56f222
kumarmo2 <kr.mohit6794@gmail.com>
2023-03-05 20:45:23 +0530

    some more changes

--- backend/src/tower_services/mod.rs
+++ backend/src/tower_services/mod.rs
@@ -3,6 +3,7 @@
 use std::pin::Pin;
 use std::{future::Future, time::Duration};
 
+use crate::models::User;
 use axum::body::Body;
 use axum::http::request::Request;
 use axum::response::IntoResponse;
@@ -35,8 +36,10 @@ impl<T, B> Service<Request<B>> for UserService<T>
 where
     T: Service<Request<B>>,
     <T as Service<Request<B>>>::Future: Send + 'static,
+    <T as Service<Request<B>>>::Response: IntoResponse,
 {
-    type Response = T::Response;
+    type Response = (Option<CookieJar>, T::Response);
+    // type Response = ;
 
     type Error = T::Error;
 
@@ -50,23 +53,35 @@ where
         return self.inner_service.poll_ready(cx);
     }
 
-    fn call(&mut self, req: Request<B>) -> Self::Future {
+    fn call(&mut self, mut req: Request<B>) -> Self::Future {
         println!("inside call");
         let mut jar = CookieJar::from_headers(req.headers());
-        if let Some(_) = jar.get("user") {
+        if let Some(user_cookie) = jar.get("user") {
+            println!("found cookie, inserting the user extension");
+            let user = User::from_str(user_cookie.value());
+            req.extensions_mut().insert(user);
             let future = self.inner_service.call(req);
-            return Box::pin(future);
-        }
-        let user_id = rusty_ulid::generate_ulid_string();
-        let user_cookie = Cookie::new("user", user_id);
-        let cookie = user_cookie.to_string();
-        println!("cookie: {}", cookie);
-        std::thread::sleep(Duration::from_secs(2));
-        // Box::pin(tokio::time::sleep(Duration::from_secs(2)));
-        // jar = jar.add(user_cookie);
-        // jar.into
-        // req.headers_mut().
+            let fut = async {
+                if let Ok(res) = future.await {
+                    let result: Result<Self::Response, Self::Error> = Ok((None, res));
+                    return result;
+                    // return Box::pin(result);
+                }
+                todo!();
+            };
+            return Box::pin(fut);
 
-        panic!()
+            // return Box::pin(future);
+        }
+        todo!();
+        let user_id = rusty_ulid::generate_ulid_string();
+        std::thread::sleep(Duration::from_secs(2));
+
+        println!("didn't find cookie, inserting into extension");
+        req.extensions_mut()
+            .insert(User::from_str(user_id.as_str()));
+
+        // let fut = self.inner_service.call(req);
+        // Box::pin(fut)
     }
 }
